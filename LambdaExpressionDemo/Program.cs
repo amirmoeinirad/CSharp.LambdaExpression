ee2e0d8f1594ea727d8d232663b64a1137cb8608
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-08-30 16:04:24 +0330

    First Commit

---
+++ LambdaExpressionDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// Amir Moeini Rad
+// May 3, 2025
+
+// Main Concept: Demonstrating the use of lambda expressions in C#
+
+namespace LambdaExpressionDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Working with Lambda Expressions in C#.NET...");
+            Console.WriteLine("--------------------------------------------\n");
+
+            // Lambda expression to add two numbers
+            // 'add' is a delegate that takes two integers and returns their sum.
+            // Therefore, in practice, we are defining an anonymous method.
+            // 'Func<>' is a built-in delegate type in C# that represents a method that returns a value.
+            // Lambda expressions are usually used to create delegates or expression tree types.
+            Func<int, int, int> add = (a, b) => a + b;
+
+
+            Console.WriteLine($"Sum: {add(5, 3)}");
+
+
+            Console.WriteLine("\nDone.");
+        }
+    }
+}
